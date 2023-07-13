65e201ed45fb52fad1f2834d9f298b9aed117246
Dex9999 <95934184+Dex9999@users.noreply.github.com>
2023-07-13 16:17:48 -0400

    Update index.js

--- index.js
+++ index.js
@@ -7,12 +7,12 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, 'public')));
 
 //redirect path to hash
-app.use((req, res, next) => {
-  if (!req.originalUrl.includes("#")) {
-    req.url = `/#${req.originalUrl.slice(1)}`;
-  }
-  next();
-});
+// app.use((req, res, next) => {
+//   if (!req.originalUrl.includes("#")) {
+//     req.redirect = `/#${req.originalUrl.slice(1)}`;
+//   }
+//   next();
+// });
 
 //for any path give index.html (doesn't work?)
 app.get('*', (req, res) => {
