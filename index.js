c35e16ebd2e4642990f22c26ea6b845fea5869b8
Dex9999 <95934184+Dex9999@users.noreply.github.com>
2023-07-13 12:32:00 -0400

    Update index.js

--- index.js
+++ index.js
@@ -4,15 +4,22 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-// Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Route all requests to index.html
+//redirect path to hash
+app.use((req, res, next) => {
+  if (!req.originalUrl.includes("#")) {
+    const redirectUrl = `https://the-password-game.vercel.app/#${req.originalUrl}`;
+    return res.redirect(301, redirectUrl);
+  }
+  next();
+});
+
+//for any path give index.html (doesn't work?)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
-// Start the server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
