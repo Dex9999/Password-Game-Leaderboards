4fc96085d1e9a9acd204ab176cbb74a10a2475d4
Dex9999 <95934184+Dex9999@users.noreply.github.com>
2023-07-13 01:42:14 -0400

    Update index.js

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Route all requests to index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'public/index.html'));
 });
 
 // Start the server
