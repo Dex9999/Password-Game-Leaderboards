3601ceb6427df21f08caee8504d5fa35c8c8a3e2
Dex9999 <95934184+Dex9999@users.noreply.github.com>
2023-07-13 01:15:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = process.env.PORT || 3000;
 // app.use(express.static(path.join(__dirname, 'public')));
 
 // Route all requests to index.html
-app.get('/*', (req, res) => {
+app.use('*', (req, res) => {
   res.sendFile(path.join('index.html'));
 });
 
